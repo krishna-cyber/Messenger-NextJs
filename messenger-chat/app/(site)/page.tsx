08bd9b210988f85537f7d7a1c6a503341ed3d7cd
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-09-06 13:42:03 +0545

    setup chakra and tailwind with next

---
+++ messenger-chat/app/(site)/page.tsx
@@ -0,0 +1,11 @@
+/** @format */
+"use client";
+import { Card } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Card>
+      <p className='text-sky-400 text-2xl'>Hello </p>
+    </Card>
+  );
+}
